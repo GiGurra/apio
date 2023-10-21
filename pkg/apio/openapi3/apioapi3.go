16b4b6a6c20c81cf16263345393515c1d829234d
Johan Kjölhede <gigurra@gmail.com>
2023-10-22 00:33:36 +0200

    wip

--- pkg/apio/openapi3/apioapi3.go
+++ pkg/apio/openapi3/apioapi3.go
@@ -251,7 +251,9 @@ func GetComponents(api apio.Api) map[string]any {
 }
 
 func schemaNameOf(structInfo apio.AnalyzedStruct) string {
-	raw := structInfo.Pkg + "/" + structInfo.Name
+	pkgParts := strings.Split(structInfo.Pkg, "/")
+	lastPart := pkgParts[len(pkgParts)-1]
+	raw := lastPart + "/" + structInfo.Name
 	// keep only alphanumeric characters, underscores and '-'s. Replace all other characters with '_'
 	return strings.Map(func(r rune) rune {
 		if r == '-' || r == '_' || (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= 9) {
